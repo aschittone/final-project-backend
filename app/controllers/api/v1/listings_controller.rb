e9809303003eedee1103df54b64e40076703193a
github username <github email address>
2017-10-02 22:26:37 -0400

    changes made

--- app/controllers/api/v1/listings_controller.rb
+++ app/controllers/api/v1/listings_controller.rb
@@ -1,9 +1,6 @@
 class Api::V1::ListingsController < ApplicationController
 
 	def get_listing
-		# receive props from front end get request
-		# send EXCON get request to zillow, and process data
-		# send back to front end
 		url_address = split_address(listing_params["address"])	
 		response = Excon.get("http://www.zillow.com/webservice/GetDeepSearchResults.htm?zws-id=X1-ZWz1fzorn263gr_9abc1&address=#{url_address[0]}&citystatezip=#{url_address[1]}%2C+#{url_address[2]}&rentzestimate=true")
 		response_hash = Hash.from_xml(response.body)
@@ -11,23 +8,28 @@ class Api::V1::ListingsController < ApplicationController
 			z_property_id = response_hash["searchresults"]["response"]["results"]["result"][0]["zpid"]
 			result = get_sort_details(z_property_id, response_hash)
 			final_result = get_details(result)
-		# byebug
 			render json: final_result
 		elsif response_hash["searchresults"]["response"]["results"]["result"].class == Array
 			result = []
 			response_hash["searchresults"]["response"]["results"]["result"].map do |property|
 				result << property["address"]["street"] + ", " + property["address"]["city"] + ", " + property["address"]["state"]
 			end
-		# byebug
-			
 			render json: result
 		else
-			z_property_id = response_hash["searchresults"]["response"]["results"]["result"]["zpid"]
-			result = get_sort_details(z_property_id, response_hash)
-			final_result = get_details(result)
-		# byebug
-			
-			render json: final_result
+			street = response_hash["searchresults"]["response"]["results"]["result"]["address"]["street"]
+			city = response_hash["searchresults"]["response"]["results"]["result"]["address"]["city"]
+			state = response_hash["searchresults"]["response"]["results"]["result"]["address"]["state"]
+			if url_address.length < 4 && city.gsub(/ /, '+') != url_address[1] 
+				result = []
+				result << "#{street}, #{city}, #{state}"
+				render json: result
+			else
+	
+				z_property_id = response_hash["searchresults"]["response"]["results"]["result"]["zpid"]
+				result = get_sort_details(z_property_id, response_hash)
+				final_result = get_details(result)
+				render json: final_result
+			end
 		end
 	end
 
@@ -35,7 +37,6 @@ class Api::V1::ListingsController < ApplicationController
 		rent_estimate = nil
 		comps = Excon.get("http://www.zillow.com/webservice/GetDeepComps.htm?zws-id=X1-ZWz1fzorn263gr_9abc1&zpid=#{z_property_id}&count=4&rentzestimate=true")
 		comps_hash = Hash.from_xml(comps.body)
-		# byebug
 		if original_results["searchresults"]["response"]["results"]["result"].class == Hash && original_results["searchresults"]["response"]["results"]["result"]["rentzestimate"] == nil
 			count = 0
 			sum = 0		
@@ -50,12 +51,16 @@ class Api::V1::ListingsController < ApplicationController
 			rent_estimate = original_results["searchresults"]["response"]["results"]["result"][0]["rentzestimate"]["amount"].to_i	
 		else 	
 			rent_estimate = original_results["searchresults"]["response"]["results"]["result"]["rentzestimate"]["amount"].to_i				
-		end	
-				
+		end			
 			if comps_hash["comps"]["message"]["code"] == "504" || comps_hash["comps"]["message"]["code"] == "503"
 				actual_comps = comps_hash["comps"]["message"]["text"]
 			else
-				actual_comps = comps_hash["comps"]["response"]["properties"]["comparables"]["comp"]	
+				if comps_hash["comps"]["response"]["properties"]["comparables"]["comp"].class == Hash
+					actual_comps = []
+					actual_comps.push(comps_hash["comps"]["response"]["properties"]["comparables"]["comp"])
+				else 
+					actual_comps = comps_hash["comps"]["response"]["properties"]["comparables"]["comp"]
+				end
 			end
 			if original_results["searchresults"]["response"]["results"]["result"].class == Array
 				return [original_results["searchresults"]["response"]["results"]["result"][0], actual_comps, rent_estimate]
@@ -70,12 +75,10 @@ class Api::V1::ListingsController < ApplicationController
 		city = results[0]["address"]["city"]
 		state = results[0]["address"]["state"]
 		data = Excon.get("https://search.onboard-apis.com/propertyapi/v1.0.0/allevents/detail?address1=#{full_street}&address2=#{city} #{state}", headers: {Accept: 'application/json',APIKey: 'f137e17eb6b1e71808a39da5419874bb'})
-		if JSON.parse(data[:body])["property"].length >= 1 || JSON.parse(data[:body])["status"]["msg"] != "SuccessWithoutResult"
-		# byebug
-			return results << data
-		else
-		# byebug			
+		if JSON.parse(data[:body])["status"]["msg"] == "Geocoder Results Address Not Identified." || JSON.parse(data[:body])["status"]["msg"] == "SuccessWithoutResult"
 			return results << "tax data not available"
+		else			
+			return results << data
 		end
 	end
 
@@ -85,7 +88,11 @@ class Api::V1::ListingsController < ApplicationController
 		street_address = split_address[0].gsub(/ /, '+')
 		city = split_address[1].gsub(/ /, '+')
 		state = split_address[2].slice(0, 2)
-		return [street_address, city, state]
+		if split_address[split_address.length - 1].include?("second")
+			return [street_address, city, state, "second"]
+		else
+			return [street_address, city, state]
+		end
 	end
 
 	private 
--- app/controllers/api/v1/user_listings_controller.rb
+++ app/controllers/api/v1/user_listings_controller.rb
@@ -1,18 +1,22 @@
 class Api::V1::UserListingsController < ApplicationController
 	
 	def create
-		address = params["0"]["address"]["street"] + ", " + params["0"]["address"]["city"] + ", " + params["0"]["address"]["state"]
-		lng = params["0"]["address"]["longitude"]
-		lat = params["0"]["address"]["latitude"]
-		rent = params["2"]
-		taxes = JSON.parse(params["3"]["data"]["body"])["property"][0]["assessment"]["tax"]["taxamt"]
-		@listing = Listing.find_or_create_by(rent: rent, address: address, taxes: taxes, latitude: lat, longitude: lng)
-		user_listings = UserListing.all.select { |user_listing| user_listing.listing_id == @listing.id && user_listing.user_id == current_user.id}
-		if user_listings.length < 1 && @listing.save
-			UserListing.create(user_id: current_user.id, listing_id: @listing.id)
-			render json: {msg: 'Listing Saved'}
-		else 
-			render json: {msg: 'Listing Already Saved'}
+		if current_user == "hello from current User"
+			render json: {msg: 'You must be logged in to save'}
+		else
+			address = params["0"]["address"]["street"] + ", " + params["0"]["address"]["city"] + ", " + params["0"]["address"]["state"]
+			lng = params["0"]["address"]["longitude"]
+			lat = params["0"]["address"]["latitude"]
+			rent = params["2"]
+			taxes = JSON.parse(params["3"]["data"]["body"])["property"][0]["assessment"]["tax"]["taxamt"]
+			@listing = Listing.find_or_create_by(rent: rent, address: address, taxes: taxes, latitude: lat, longitude: lng)
+			user_listings = UserListing.all.select { |user_listing| user_listing.listing_id == @listing.id && user_listing.user_id == current_user.id}
+			if user_listings.length < 1 && @listing.save
+				UserListing.create(user_id: current_user.id, listing_id: @listing.id)
+				render json: {msg: 'Listing Saved'}
+			else 
+				render json: {msg: 'Listing Already Saved'}
+			end
 		end
 	end
 
